5613cb1e0ee06d18210ffaafcc32401081ab67df
rhkralsgh <knh239@naver.com>
2025-11-12 15:07:03 +0900

    Initial Commit

---
+++ S25W11List/S25W11ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  S25W11ListApp.swift
+//  S25W11List
+//
+//  Created by student on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct S25W11ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ S25W11ListTests/S25W11ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  S25W11ListTests.swift
+//  S25W11ListTests
+//
+//  Created by student on 11/12/25.
+//
+
+import Testing
+@testable import S25W11List
+
+struct S25W11ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
