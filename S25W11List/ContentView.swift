5ea703fc67c6dcfdec3476d2867345b5138a8cf3
rhkralsgh <knh239@naver.com>
2025-11-12 15:13:36 +0900

    ✨List

--- S25W11List/ContentView.swift
+++ S25W11List/ContentView.swift
@@ -2,13 +2,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MyList()
+    }
+}
+
+struct MyList: View {
+    var body: some View {
+        List {
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
+            Text("노래 1")
+            Text("노래 2")
+            Text("노래 3")
         }
-        .padding()
     }
 }
 
