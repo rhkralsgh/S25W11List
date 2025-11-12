0c2436c30d6376963cbfad78e60523f002543a49
rhkralsgh <knh239@naver.com>
2025-11-12 15:50:28 +0900

    🎨컴포넌트 분리

--- S25W11List/ContentView.swift
+++ S25W11List/ContentView.swift
@@ -12,16 +12,7 @@ struct MyList: View {
     var body: some View {
         List {
             ForEach(viewModel.songs, id: \.id) { song in
-                VStack {
-                    Text(song.title)
-                        .font(.headline)
-                        .foregroundColor(.blue)
-                        .padding()
-                    Text(song.singer)
-                        .font(.caption)
-                        .foregroundColor(.gray)
-                        .padding()
-                }
+                SongItem(song: song)
             }
         }
         .onAppear {
@@ -29,6 +20,37 @@ struct MyList: View {
             viewModel.add(song: Song(id: 2, title: "Drowning", singer: "WOODZ"))
             viewModel.add(song: Song(id: 3, title: "Soda Pop", singer: "Saja Boys"))
         }
+    }
+}
+
+struct SongItem: View {
+    let song: Song
+    var body: some View {
+        VStack {
+            TextTitle(title: song.title)
+            TextSinger(singer: song.singer)
+        }
+        .padding()
+    }
+}
+
+struct TextTitle: View {
+    let title: String
+    var body: some View {
+        Text(title)
+            .font(.headline)
+            .foregroundColor(.blue)
+            .padding()
+    }
+}
+
+struct TextSinger: View {
+    let singer: String
+    var body: some View {
+        Text(singer)
+            .font(.caption)
+            .foregroundColor(.gray)
+            .padding()
     }
 }
 
